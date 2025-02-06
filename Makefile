4fbad937d047eab8bc30d06a193c6266797aa3a1
Alan Lionel Figueredo Aguiar <lionelfigueredo@gmail.com>
2025-02-05 23:04:17 -0300

    Init snake

---
+++ Makefile
@@ -0,0 +1,34 @@
+# Globales
+CC := g++
+CFLAGS := -Wall
+MODULE_NAME := ./build/main.out
+SOURCE_FILES := $(shell find . -type f -iname "*.cpp" -exec basename {} \;)
+OBJECT_FILES := $(SOURCE_FILES:%.cpp=./build/%.o)
+LINKS := -lsfml-graphics -lsfml-window -lsfml-system
+TEST_FILE := ./test.txt
+
+### Reglas ###
+
+build: makefolder $(MODULE_NAME)
+
+debug: CFLAGS := $(CFLAGS) -g
+debug: makeshared_debug build
+
+rebuild: clean build
+
+test: build
+	$(MODULE_NAME) $(MAIN_FILE)
+
+$(MODULE_NAME): $(OBJECT_FILES)
+	$(CC) $(CFLAGS) -o $@ $^ $(LINKS)
+
+./build/%.o: ./%.cpp
+	$(CC) $(CFLAGS) -c $< -o $@
+
+makefolder:
+	if [ ! -d ./build ]; then mkdir build; fi
+
+# Clean
+
+clean:
+	rm -rf build
---
+++ main.cpp
@@ -0,0 +1,143 @@
+#include <SFML/Graphics.hpp>
+#include <deque>
+#include <iostream>
+#include <random>
+
+sf::RectangleShape new_shape(sf::Vector2f pos, const float size) {
+  sf::RectangleShape shape(sf::Vector2f(size, size));
+  shape.setFillColor(sf::Color::Yellow);
+  shape.setPosition(pos * size);
+  return shape;
+}
+
+int main() {
+  const unsigned int widthP = 512;
+  const unsigned int heightP = 256;
+  const float size = 32;
+
+  const unsigned int width = widthP / size;
+  const unsigned int height = heightP / size;
+
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_int_distribution<> randWidth(0, width - 1);
+  std::uniform_int_distribution<> randHeight(0, height - 1);
+
+  sf::RenderWindow window(sf::VideoMode({widthP, heightP}), "SFML works!");
+
+  std::deque<sf::RectangleShape> snake;
+  for (unsigned i = 0; i < 3; ++i)
+    snake.push_front(new_shape(sf::Vector2f(i, 0), size));
+  snake[0].setFillColor(sf::Color::Green);
+  snake[1].setFillColor(sf::Color::Green);
+
+  sf::RectangleShape food(sf::Vector2f(size, size));
+  food.setFillColor(sf::Color::White);
+  food.setPosition(sf::Vector2f(size * randWidth(gen), size * randHeight(gen)));
+  while (std::any_of(snake.begin(), snake.end(),
+                     [&food](sf::RectangleShape &shape) {
+                       return food.getPosition() == shape.getPosition();
+                     }))
+    food.setPosition(
+        sf::Vector2f(size * randWidth(gen), size * randHeight(gen)));
+
+  sf::Keyboard::Scan::Scancode lastMoving = sf::Keyboard::Scan::D;
+  sf::Keyboard::Scan::Scancode moving = sf::Keyboard::Scan::D;
+  bool lose = false;
+
+  while (window.isOpen()) {
+    sf::Event event;
+    const int pause =
+        sf::Keyboard::isKeyPressed(sf::Keyboard::Scan::X) ? 100 : 1000;
+    while (window.pollEvent(event)) {
+      std::cout << "TRACE: event: " << event.type << '\n';
+      if (event.type == sf::Event::Closed)
+        window.close();
+      else if (event.type == sf::Event::KeyPressed) {
+        switch (event.key.scancode) {
+        case sf::Keyboard::Scan::W:
+          if (lastMoving != sf::Keyboard::Scan::S)
+            moving = event.key.scancode;
+          break;
+        case sf::Keyboard::Scan::S:
+          if (lastMoving != sf::Keyboard::Scan::W)
+            moving = event.key.scancode;
+          break;
+        case sf::Keyboard::Scan::A:
+          if (lastMoving != sf::Keyboard::Scan::D)
+            moving = event.key.scancode;
+          break;
+        case sf::Keyboard::Scan::D:
+          if (lastMoving != sf::Keyboard::Scan::A)
+            moving = event.key.scancode;
+          break;
+        default:
+          break;
+        }
+      }
+    }
+    if (lose) {
+      sf::sleep(sf::milliseconds(pause));
+      continue;
+    }
+
+    std::cout << "TRACE: moving: " << moving << '\n';
+    const sf::Vector2f pos = snake.front().getPosition() / size;
+    sf::Vector2f move = sf::Vector2f(-1, -1);
+    switch (moving) {
+    case sf::Keyboard::Scan::W:
+      move = pos +
+             sf::Vector2f(0, pos.y != 0 ? -1 : static_cast<float>(height) - 1);
+      break;
+    case sf::Keyboard::Scan::S:
+      move = pos + sf::Vector2f(0, pos.y != height - 1
+                                       ? 1
+                                       : 1 - static_cast<float>(height));
+      break;
+    case sf::Keyboard::Scan::A:
+      move = pos +
+             sf::Vector2f(pos.x != 0 ? -1 : static_cast<float>(width) - 1, 0);
+      break;
+    case sf::Keyboard::Scan::D:
+      move =
+          pos + sf::Vector2f(
+                    pos.x != width - 1 ? 1 : 1 - static_cast<float>(width), 0);
+      break;
+    default:
+      break;
+    }
+    std::cout << "TRACE: move: " << move.x << ", " << move.y << '\n';
+    if (std::any_of(snake.begin(), snake.end() - 1,
+                    [&move, &size](sf::RectangleShape &shape) {
+                      return move == shape.getPosition() / size;
+                    })) {
+      for (auto &shape : snake)
+        shape.setFillColor(sf::Color::Red);
+      lose = true;
+    }
+    if (!lose && move != sf::Vector2f(-1, -1)) {
+      snake.push_front(new_shape(move, size));
+      snake[1].setFillColor(sf::Color::Green);
+      lastMoving = moving;
+      if (move == food.getPosition() / size) {
+        food.setPosition(size * randWidth(gen), size * randHeight(gen));
+        while (std::any_of(snake.begin(), snake.end(),
+                           [&food](sf::RectangleShape &shape) {
+                             return food.getPosition() == shape.getPosition();
+                           }))
+          food.setPosition(
+              sf::Vector2f(size * randWidth(gen), size * randHeight(gen)));
+      } else
+        snake.pop_back();
+    }
+    window.clear();
+    for (const auto &shape : snake)
+      window.draw(shape);
+    window.draw(food);
+    window.display();
+
+    sf::sleep(sf::milliseconds(pause));
+  }
+
+  return 0;
+}
